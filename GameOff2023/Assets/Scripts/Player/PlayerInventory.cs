35396e7930321873e594e2670465a3c959b1131a
JesseTimonen <jesse.timonen@outlook.com>
2023-11-24 10:12:52 +0200

    Set inventory UI element to be hidden incase their value is 0

--- GameOff2023/Assets/Scripts/Player/PlayerInventory.cs
+++ GameOff2023/Assets/Scripts/Player/PlayerInventory.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+using static PurchaseManager;
 using static UnityEditor.Progress;
 using static UnityEngine.Rendering.DebugUI;
 
@@ -165,12 +166,16 @@ public class PlayerInventory : MonoBehaviour
     private void UpdateTorchesUI()
     {
         torchesText.text = torches.ToString();
+        torchesText.gameObject.SetActive(torches > 0);
+        torchesText.transform.parent.gameObject.SetActive(torches > 0);
     }
 
 
     private void UpdateStaminaPotionsUI()
     {
         staminaPotionsText.text = staminaPotions.ToString();
+        staminaPotionsText.gameObject.SetActive(staminaPotions > 0);
+        staminaPotionsText.transform.parent.gameObject.SetActive(staminaPotions > 0);
     }
 
 
@@ -181,15 +186,23 @@ public class PlayerInventory : MonoBehaviour
             switch (item.Ground)
             {
                 case Grounds.Dirt:
+                    dirtText.gameObject.SetActive(item.Amount > 0);
+                    dirtText.transform.parent.gameObject.SetActive(item.Amount > 0);
                     dirtText.text = item.Amount.ToString();
                     break;
                 case Grounds.Stone:
+                    stoneText.gameObject.SetActive(item.Amount > 0);
+                    stoneText.transform.parent.gameObject.SetActive(item.Amount > 0);
                     stoneText.text = item.Amount.ToString();
                     break;
                 case Grounds.Bedrock:
+                    bedrockText.gameObject.SetActive(item.Amount > 0);
+                    bedrockText.transform.parent.gameObject.SetActive(item.Amount > 0);
                     bedrockText.text = item.Amount.ToString();
                     break;
                 case Grounds.DragonStone:
+                    dragonStoneText.gameObject.SetActive(item.Amount > 0);
+                    dragonStoneText.transform.parent.gameObject.SetActive(item.Amount > 0);
                     dragonStoneText.text = item.Amount.ToString();
                     break;
             }
@@ -204,13 +217,19 @@ public class PlayerInventory : MonoBehaviour
             switch (item.Valuable)
             {
                 case Valuables.Gold:
+                    goldText.gameObject.SetActive(item.Amount > 0);
+                    goldText.transform.parent.gameObject.SetActive(item.Amount > 0);
                     goldText.text = item.Amount.ToString();
                     break;
                 case Valuables.Amethyst:
                     amethystText.text = item.Amount.ToString();
+                    amethystText.gameObject.SetActive(item.Amount > 0);
+                    amethystText.transform.parent.gameObject.SetActive(item.Amount > 0);
                     break;
                 case Valuables.Diamond:
                     diamondText.text = item.Amount.ToString();
+                    diamondText.gameObject.SetActive(item.Amount > 0);
+                    diamondText.transform.parent.gameObject.SetActive(item.Amount > 0);
                     break;
             }
         }
